b98a8f50ee066cc3864da8459dbc389dc05eeecc
Arne <fostvedt.arne@gmail.com>
2016-04-04 04:00:22 -0400

    refactored a bit

--- meteor-demo/server/main.js
+++ meteor-demo/server/main.js
@@ -4,3 +4,8 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+var Markers = new Meteor.Collection('markers');
+Meteor.publish("markers", function () {
+  return Markers.find();
+});
